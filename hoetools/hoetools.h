aca475710844385de6bf708270167326acb72299
vincent78 <vincent78@163.com>
2015-12-02 19:21:11 +0800

    project:初始化

---
+++ hoetools/hoetools.h
@@ -0,0 +1,19 @@
+//
+//  hoetools.h
+//  hoetools
+//
+//  Created by vincent on 15/12/2.
+//  Copyright © 2015年 fruit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for hoetools.
+FOUNDATION_EXPORT double hoetoolsVersionNumber;
+
+//! Project version string for hoetools.
+FOUNDATION_EXPORT const unsigned char hoetoolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <hoetools/PublicHeader.h>
+
+
